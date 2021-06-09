4af87fa8707252a0116279a607c203b8dcf4c5d9
andrez17 <36645686+andrez17@users.noreply.github.com>
2021-06-09 12:50:44 -0400

    Add files via upload

---
+++ flexbox/main.js
@@ -0,0 +1,14 @@
+// Foursquare API Info
+const clientId = 'MWP0UKP00FKFGP5PGJTF3ZVZH5FZO5ZQ3LZ4VGWBGWBCQW3F';
+const clientSecret = 'VDVGG3TGNHZPJMQLDKCDPYUB1IMPPJAOXFDF22G32QKKCOCZ';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+// OpenWeather Info
+const openWeatherKey = '3e4db191413c41a373c3d3ed2840d8ff';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
